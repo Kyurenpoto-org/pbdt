ea8375a24a454728e23f2df94917c598ff13f27d
Kyurenpoto <heal9179@gmail.com>
2025-03-07 04:32:20 +0900

    [build] Change message types

--- cmake/conan.cmake
+++ cmake/conan.cmake
@@ -34,7 +34,7 @@ set(conan_toolchain_path ${CMAKE_BINARY_DIR}/conan_installed/build/${CMAKE_BUILD
 if(EXISTS ${conan_toolchain_path})
     include(${conan_toolchain_path})
 else()
-    message(WARNING "Conan toolchain file not found at ${conan_toolchain_path}. Check your Conan installation. See https://docs.conan.io/2/installation.html for more information.")
+    message(FATAL_ERROR "Conan toolchain file not found at ${conan_toolchain_path}. Check your Conan installation. See https://docs.conan.io/2/installation.html for more information.")
 endif()
 
 unset(conan_toolchain_path)
