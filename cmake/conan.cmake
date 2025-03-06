a0c11e476cdde695148b30b7554b378dd403a874
Kyurenpoto <heal9179@gmail.com>
2025-03-07 04:11:25 +0900

    [build] Provide more specific links

--- cmake/conan.cmake
+++ cmake/conan.cmake
@@ -34,7 +34,7 @@ set(conan_toolchain_path ${CMAKE_BINARY_DIR}/conan_installed/build/${CMAKE_BUILD
 if(EXISTS ${conan_toolchain_path})
     include(${conan_toolchain_path})
 else()
-    message(WARNING "Conan toolchain file not found at ${conan_toolchain_path}. Check your Conan installation and ensure that the Conan profile is correctly configured. See https://docs.conan.io for more information.")
+    message(WARNING "Conan toolchain file not found at ${conan_toolchain_path}. Check your Conan installation and ensure that the Conan profile is correctly configured. See https://docs.conan.io/2/reference/config_files/profiles.html#profiles for more information.")
 endif()
 
 unset(conan_toolchain_path)
