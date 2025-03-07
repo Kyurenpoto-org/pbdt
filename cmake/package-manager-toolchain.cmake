55d5a2b776438f2478e5605782d5a56760e4be2d
Kyurenpoto <heal9179@gmail.com>
2025-03-07 23:28:29 +0900

    [chore] Squash from `dev` branch

--- cmake/package-manager-toolchain.cmake
+++ cmake/package-manager-toolchain.cmake
@@ -20,8 +20,16 @@ else()
 endif()
 
 # conan
-if(DEFINED ENV{CONAN_HOME})
-	include(${CMAKE_CURRENT_LIST_DIR}/conan.cmake)
+execute_process(
+	COMMAND conan --version
+	RESULT_VARIABLE conan_result
+	OUTPUT_VARIABLE conan_output
+	ERROR_VARIABLE conan_error
+	OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+if(conan_result OR NOT conan_output MATCHES "^Conan version [0-9]+\\.[0-9]+\\.[0-9]+.*$")
+	message(FATAL_ERROR "Conan is not installed or the version string format is unexpected. Check your Conan installation. See https://docs.conan.io/2/installation.html for more information.")
 else()
-	message(FATAL_ERROR "CONAN_HOME not defined. Please set the CONAN_HOME environment variable to the location of your Conan installation. You can set it in your system environment variables. See https://docs.conan.io/2/reference/environment.html#conan-home for more information.")
+	include(${CMAKE_CURRENT_LIST_DIR}/conan.cmake)
 endif()
