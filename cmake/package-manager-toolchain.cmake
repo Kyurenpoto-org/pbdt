71dfabb66afb4527a2b736e0b28f9d1b4a443c8e
Kyurenpoto <heal9179@gmail.com>
2025-03-07 22:47:38 +0900

    [build] Fix version string regex

--- cmake/package-manager-toolchain.cmake
+++ cmake/package-manager-toolchain.cmake
@@ -28,8 +28,8 @@ execute_process(
 	OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
-if(conan_result OR NOT conan_output MATCHES "^Conan version [0-9]+\\.[0-9]+\\.[0-9]+")
-	message(FATAL_ERROR "Conan is not installed. Check your Conan installation. See https://docs.conan.io/2/installation.html for more information.")
+if(conan_result OR NOT conan_output MATCHES "^Conan version [0-9]+\\.[0-9]+\\.[0-9]+.*$")
+	message(FATAL_ERROR "Conan is not installed or the version string format is unexpected. Check your Conan installation. See https://docs.conan.io/2/installation.html for more information.")
 else()
 	include(${CMAKE_CURRENT_LIST_DIR}/conan.cmake)
 endif()
