778c20d1aa17925f9a64a8c04699b041712f766d
Kyurenpoto <heal9179@gmail.com>
2025-03-14 00:21:35 +0900

    [build|ci] integrate (#36)
    #13

--- cmake/package-manager-toolchain.cmake
+++ cmake/package-manager-toolchain.cmake
@@ -10,6 +10,16 @@ if(DEFINED ENV{VCPKG_ROOT})
 endif()
 
 # conan
-if(DEFINED ENV{CONAN_HOME})
+execute_process(
+	COMMAND conan --version
+	RESULT_VARIABLE conan_result
+	OUTPUT_VARIABLE conan_output
+	ERROR_VARIABLE conan_error
+	OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+if(conan_result OR NOT conan_output MATCHES "^Conan version [0-9]+\\.[0-9]+\\.[0-9]+.*$")
+	message(FATAL_ERROR "Conan is not installed or the version string format is unexpected. Check your Conan installation. See https://docs.conan.io/2/installation.html for more information.")
+else()
 	include(${CMAKE_CURRENT_LIST_DIR}/conan.cmake)
 endif()
