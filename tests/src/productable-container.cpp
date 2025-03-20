e0f977ed124b2a4ce7076a99242578d0057e6f6a
Kyurenpoto <heal9179@gmail.com>
2025-03-21 08:57:58 +0900

    [test] Fix for gcc, clang

--- tests/src/productable-container.cpp
+++ tests/src/productable-container.cpp
@@ -12,30 +12,30 @@
 
 #include "productable-container.hpp"
 
-ProductableContainer<0, 0>;
-ProductableContainer<0, 1>;
-ProductableContainer<0, 2>;
-ProductableContainer<0, 3>;
-ProductableContainer<0, 4>;
-ProductableContainer<0, 5>;
+template class ProductableContainer<0, 0>;
+template class ProductableContainer<0, 1>;
+template class ProductableContainer<0, 2>;
+template class ProductableContainer<0, 3>;
+template class ProductableContainer<0, 4>;
+template class ProductableContainer<0, 5>;
 
-ProductableContainer<1, 0>;
-ProductableContainer<1, 1>;
-ProductableContainer<1, 2>;
-ProductableContainer<1, 3>;
-ProductableContainer<1, 4>;
-ProductableContainer<1, 5>;
+template class ProductableContainer<1, 0>;
+template class ProductableContainer<1, 1>;
+template class ProductableContainer<1, 2>;
+template class ProductableContainer<1, 3>;
+template class ProductableContainer<1, 4>;
+template class ProductableContainer<1, 5>;
 
-ProductableContainer<2, 0>;
-ProductableContainer<2, 1>;
-ProductableContainer<2, 2>;
-ProductableContainer<2, 3>;
-ProductableContainer<2, 4>;
-ProductableContainer<2, 5>;
+template class ProductableContainer<2, 0>;
+template class ProductableContainer<2, 1>;
+template class ProductableContainer<2, 2>;
+template class ProductableContainer<2, 3>;
+template class ProductableContainer<2, 4>;
+template class ProductableContainer<2, 5>;
 
-ProductableContainer<3, 0>;
-ProductableContainer<3, 1>;
-ProductableContainer<3, 2>;
-ProductableContainer<3, 3>;
-ProductableContainer<3, 4>;
-ProductableContainer<3, 5>;
+template class ProductableContainer<3, 0>;
+template class ProductableContainer<3, 1>;
+template class ProductableContainer<3, 2>;
+template class ProductableContainer<3, 3>;
+template class ProductableContainer<3, 4>;
+template class ProductableContainer<3, 5>;
