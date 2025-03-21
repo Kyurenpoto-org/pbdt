184d4cd557cdae34f1a440dc2de13de1fcaebf4c
Kyurenpoto <heal9179@gmail.com>
2025-03-22 00:01:01 +0900

    [test] Add module tests for then component

---
+++ tests/src/foldable-callable.cpp
@@ -0,0 +1,25 @@
+/*
+ * SPDX - FileCopyrightText: © 2025 Kyurenpoto <heal9179@gmail.com>
+ *
+ * SPDX - License - Identifier: MIT
+ */
+
+#include "foldable-callable.hpp"
+
+template class Foldable::FoldableCallable<0, 0>;
+template class Foldable::FoldableCallable<0, 1>;
+template class Foldable::FoldableCallable<0, 2>;
+template class Foldable::FoldableCallable<0, 3>;
+template class Foldable::FoldableCallable<0, 4>;
+template class Foldable::FoldableCallable<0, 5>;
+template class Foldable::FoldableCallable<0, 6>;
+template class Foldable::FoldableCallable<0, 7>;
+
+template class Foldable::FoldableCallable<1, 0>;
+template class Foldable::FoldableCallable<1, 1>;
+template class Foldable::FoldableCallable<1, 2>;
+template class Foldable::FoldableCallable<1, 3>;
+template class Foldable::FoldableCallable<1, 4>;
+template class Foldable::FoldableCallable<1, 5>;
+template class Foldable::FoldableCallable<1, 6>;
+template class Foldable::FoldableCallable<1, 7>;
---
+++ tests/src/then-component-associative-header.cpp
@@ -0,0 +1,70 @@
+/*
+ * SPDX - FileCopyrightText: © 2024 Kyurenpoto <heal9179@gmail.com>
+ *
+ * SPDX - License - Identifier: MIT
+ */
+
+#include "then-component-associative-common.hpp"
+
+#include "pbdt/bdd.hpp"
+
+namespace
+{
+    template <typename T>
+    constexpr std::tuple<T> toTuple(const T t)
+    {
+        return { t };
+    }
+
+    template <typename... Props, typename Sample, typename Result>
+    constexpr std::tuple<Props...>
+    toTuple(const pbdt::bdd::detail::PropertyCompletion<std::tuple<Props...>, Sample, Result> completion)
+    {
+        return { completion };
+    }
+
+    constexpr auto flatTuple(auto&& tup)
+    {
+        return std::apply(
+            [](const auto&... args)
+            {
+                return std::tuple_cat(toTuple(args)...);
+            },
+            tup
+        );
+    }
+}
+
+struct Expect
+{
+    constexpr auto
+    operator()(const bool expression, const std::source_location& location = std::source_location::current())
+    {
+        return pbdt::test_context::expect(expression, location);
+    }
+};
+
+struct ToFlatTuple
+{
+    template <typename T>
+    constexpr auto operator()(T&& t) const
+    {
+        return flatTuple(toTuple(t));
+    }
+};
+
+struct Then
+{
+    template <typename Prop>
+    constexpr auto operator()(Prop&& prop) const
+    {
+        return pbdt::bdd::then("", std::forward<Prop>(prop));
+    }
+};
+
+int main()
+{
+    associative<Expect, ToFlatTuple, Then>();
+
+    return EXIT_SUCCESS;
+}
