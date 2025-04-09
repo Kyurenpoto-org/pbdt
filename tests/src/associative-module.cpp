817658919cc5b6abf9e6579f1c3fa1d74acdc833
Kyurenpoto <heal9179@gmail.com>
2025-04-09 21:15:21 +0900

    [test] Add property suites

---
+++ tests/src/associative-module.cpp
@@ -0,0 +1,40 @@
+/*
+ * SPDX - FileCopyrightText: © 2025 Kyurenpoto <heal9179@gmail.com>
+ *
+ * SPDX - License - Identifier: MIT
+ */
+
+#include <array>
+
+#include "properties/associative.hpp"
+
+import exstd;
+import pbdt;
+
+#include "for-injection.hpp"
+#include "suite-runner.hpp"
+
+void given()
+{
+    const AcceptableRawContext<TwoWayCompletableRawGivenContext<Given>> acceptable;
+    acceptable.accept(AssociativeValidator<ToFlatTuple>{});
+}
+
+void when()
+{
+    const AcceptableRawContext<TwoWayCompletableRawWhenContext<When>> acceptable;
+    acceptable.accept(AssociativeValidator<ToContainer>{});
+}
+
+void then()
+{
+    const AcceptableRawContext<TwoWayCompletableRawThenContext<Then, Expect>> acceptable;
+    acceptable.accept(AssociativeValidator<ToFlatTuple>{});
+}
+
+int main(int argc, const char* const* argv)
+{
+    suite(std::array{ "given", "when", "then" }, std::array{ given, when, then }).run(argc, argv);
+
+    return EXIT_SUCCESS;
+}
---
+++ tests/src/idempotent-module.cpp
@@ -0,0 +1,40 @@
+/*
+ * SPDX - FileCopyrightText: © 2025 Kyurenpoto <heal9179@gmail.com>
+ *
+ * SPDX - License - Identifier: MIT
+ */
+
+#include <array>
+
+#include "properties/idempotent.hpp"
+
+import exstd;
+import pbdt;
+
+#include "for-injection.hpp"
+#include "suite-runner.hpp"
+
+void given()
+{
+    const AcceptableRawContext<CompletableRawGivenContext<Given>> acceptable;
+    acceptable.accept(IdempotentValidator<Given, ToFlatTuple>{});
+}
+
+void when()
+{
+    const AcceptableRawContext<CompletableRawWhenContext<When>> acceptable;
+    acceptable.accept(IdempotentValidator<When, ToContainer>{});
+}
+
+void then()
+{
+    const AcceptableRawContext<CompletableRawThenContext<Then, Expect>> acceptable;
+    acceptable.accept(IdempotentValidator<Then, ToFlatTuple>{});
+}
+
+int main(int argc, const char* const* argv)
+{
+    suite(std::array{ "given", "when", "then" }, std::array{ given, when, then }).run(argc, argv);
+
+    return EXIT_SUCCESS;
+}
