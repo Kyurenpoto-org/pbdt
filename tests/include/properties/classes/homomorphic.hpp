821aacdeee8066b8832ca63a7b91c7e9c985d3f9
Kyurenpoto <heal9179@gmail.com>
2025-07-15 06:49:06 +0900

    [test] Add homomorphic property unit tests for event-countable

--- tests/include/properties/classes/homomorphic.hpp
+++ tests/include/properties/classes/homomorphic.hpp
@@ -91,3 +91,140 @@ private:
     static constexpr RunnableScenario runnableScenario{};
     static constexpr Then then{};
 };
+
+template <typename EventCountable>
+struct EventCountableDoubleValueCombination
+{
+    static constexpr size_t size()
+    {
+        return std::tuple_size_v<decltype(OPS)>;
+    }
+
+    template <size_t Idx>
+    constexpr auto a() const
+    {
+        return value<A[Idx]>();
+    }
+
+    template <size_t Idx>
+    constexpr auto b() const
+    {
+        return value<B[Idx]>();
+    }
+
+private:
+    template <size_t Idx>
+    constexpr auto value() const
+    {
+        if constexpr (Idx == 0)
+        {
+            return eventCountable.prototype();
+        }
+        else
+        {
+            return OPS[Idx - 1](value<Idx - 1>());
+        }
+    }
+
+    static constexpr auto OPS = std::array<typename EventCountable::OpContainer, 8>{
+        EventCountable::template Op<COMPILE_TIME_RANDOM() % 3>(),
+        EventCountable::template Op<COMPILE_TIME_RANDOM() % 3>(),
+        EventCountable::template Op<COMPILE_TIME_RANDOM() % 3>(),
+        EventCountable::template Op<COMPILE_TIME_RANDOM() % 3>(),
+        EventCountable::template Op<COMPILE_TIME_RANDOM() % 3>(),
+        EventCountable::template Op<COMPILE_TIME_RANDOM() % 3>(),
+        EventCountable::template Op<COMPILE_TIME_RANDOM() % 3>(),
+        EventCountable::template Op<COMPILE_TIME_RANDOM() % 3>(),
+    };
+    static constexpr std::array<size_t, 8> A{
+        COMPILE_TIME_RANDOM() % 8, COMPILE_TIME_RANDOM() % 8, COMPILE_TIME_RANDOM() % 8, COMPILE_TIME_RANDOM() % 8,
+        COMPILE_TIME_RANDOM() % 8, COMPILE_TIME_RANDOM() % 8, COMPILE_TIME_RANDOM() % 8, COMPILE_TIME_RANDOM() % 8,
+    };
+    static constexpr std::array<size_t, 8> B{
+        COMPILE_TIME_RANDOM() % 8, COMPILE_TIME_RANDOM() % 8, COMPILE_TIME_RANDOM() % 8, COMPILE_TIME_RANDOM() % 8,
+        COMPILE_TIME_RANDOM() % 8, COMPILE_TIME_RANDOM() % 8, COMPILE_TIME_RANDOM() % 8, COMPILE_TIME_RANDOM() % 8,
+    };
+    static constexpr EventCountable eventCountable{};
+};
+
+template <typename EventCountable>
+struct HomomorphicEventCountableSumWithAccumulateRequirements
+{
+    static constexpr size_t size()
+    {
+        return COMBINATIONS.size();
+    }
+
+    template <size_t Idx>
+    constexpr auto a() const
+    {
+        return COMBINATIONS.template a<Idx>();
+    }
+
+    template <size_t Idx>
+    constexpr auto b() const
+    {
+        return COMBINATIONS.template b<Idx>();
+    }
+
+    template <size_t, typename T>
+    constexpr size_t morph(T&& t) const
+    {
+        return std::forward<T>(t).sum();
+    }
+
+    template <typename T>
+    constexpr auto beforeMorphOp(T&& a, T&& b) const
+    {
+        return std::forward<T>(a) + std::forward<T>(b);
+    }
+
+    constexpr size_t afterMorphOp(size_t a, size_t b) const
+    {
+        return a + b;
+    }
+
+private:
+    static constexpr auto COMBINATIONS = EventCountableDoubleValueCombination<EventCountable>{};
+};
+
+template <typename EventCountable>
+struct HomomorphicEventCountableSomeFailedWithAccumulateRequirements
+{
+    static constexpr size_t size()
+    {
+        return COMBINATIONS.size();
+    }
+
+    template <size_t Idx>
+    constexpr auto a() const
+    {
+        return COMBINATIONS.template a<Idx>();
+    }
+
+    template <size_t Idx>
+    constexpr auto b() const
+    {
+        return COMBINATIONS.template b<Idx>();
+    }
+
+    template <size_t, typename T>
+    constexpr bool morph(T&& t) const
+    {
+        return std::forward<T>(t).someFailed();
+    }
+
+    template <typename T>
+    constexpr auto beforeMorphOp(T&& a, T&& b) const
+    {
+        return std::forward<T>(a) + std::forward<T>(b);
+    }
+
+    constexpr bool afterMorphOp(bool a, bool b) const
+    {
+        return a || b;
+    }
+
+private:
+    static constexpr auto COMBINATIONS = EventCountableDoubleValueCombination<EventCountable>{};
+};
