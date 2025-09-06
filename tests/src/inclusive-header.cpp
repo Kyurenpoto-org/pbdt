6fc509c508a2743f524a5114fd547b1b9296b6f8
Kyurenpoto <heal9179@gmail.com>
2025-09-07 08:54:38 +0900

    [test|obsolete] Remove previous unit tests for inclusive property

--- tests/src/inclusive-header.cpp
+++ tests/src/inclusive-header.cpp
@@ -29,14 +29,6 @@ void callableProperty()
     acceptable.run();
 }
 
-void eventCountableOld()
-{
-    const InclusiveStringValidation<
-        InclusiveEventCountableStringifyRequirements<pbdt::test_context::detail::EventCountable>>
-        acceptable;
-    acceptable.run();
-}
-
 void eventCountable()
 {
     const InclusiveStringPiecesValidation<
@@ -51,13 +43,11 @@ int main(int argc, const char* const* argv)
         std::array{
             "callable-target",
             "callable-property",
-            "event-countable-stringify-old",
             "event-countable-stringify",
         },
         std::array{
             callableTarget,
             callableProperty,
-            eventCountableOld,
             eventCountable,
         }
     )
